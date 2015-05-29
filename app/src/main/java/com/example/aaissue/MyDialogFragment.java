c18d60e5f3ed6ad9dce2e7d05c1f217047d58d6e
Junya Matsuyama <matsuyama.junya@dena.jp>
2015-05-29 19:14:36 +0900

    Commit

---
+++ app/src/main/java/com/example/aaissue/MyDialogFragment.java
@@ -0,0 +1,9 @@
+package com.example.aaissue;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by matsuyama.junya on 2015/05/29.
+ */
+public class MyDialogFragment extends DialogFragment {
+}
